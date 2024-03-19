0882d7593e84a30cc94a5b233f44e1c13bb614bf
Yukai Tu <yukai.tu@coinbase.com>
2024-03-19 13:19:50 -0700

    Add getL1FeeUpperBound to GasPriceOracle

--- packages/contracts-bedrock/src/L2/GasPriceOracle.sol
+++ packages/contracts-bedrock/src/L2/GasPriceOracle.sol
@@ -53,6 +53,26 @@ contract GasPriceOracle is ISemver {
         return _getL1FeeBedrock(_data);
     }
 
+    /// @notice returns an upper bound for the L1 fee for a given transaction size.
+    /// It is provided for callers who wish to estimate L1 transaction costs in the
+    /// write path, and is much more gas efficient than `getL1Fee`.
+    /// It assumes the worst case of fastlz upper-bound which covers %99.99 txs.
+    /// @param _unsignedTxSize Unsigned fully RLP-encoded transaction size to get the L1 fee for.
+    /// @return L1 estimated upper-bound fee that should be paid for the tx
+    function getL1FeeUpperBound(uint256 _unsignedTxSize) external view returns (uint256) {
+        require(isFjord, "GasPriceOracle: getL1FeeUpperBound only supports Fjord");
+
+        int256 txSize = int256(_unsignedTxSize) + 68;
+        uint256 feeScaled = baseFeeScalar() * 16 * l1BaseFee() + blobBaseFeeScalar() * blobBaseFee();
+        // txSize / 255 + 16 is the pratical fastlz upper-bound covers %99.99 txs.
+        int256 flzUpperBound = txSize / 255 + 16;
+        int256 cost = COST_INTERCEPT + COST_FASTLZ_COEF * flzUpperBound + COST_TX_SIZE_COEF * txSize;
+        if (cost < 0) {
+            cost = 0;
+        }
+        return uint256(cost) * feeScaled / (10 ** (DECIMALS * 2));
+    }
+
     /// @notice Set chain to be Ecotone chain (callable by depositor account)
     function setEcotone() external {
         require(
